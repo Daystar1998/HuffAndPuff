06401d55979cd0fad0805f82cb114d222cb168fb
Matthew Day <daystar.1998@yahoo.com>
2019-10-28 16:06:01 -0500

    Implement generateInitialHuffmanTable

--- Huff and Puff/Huff and Puff/src/huff.cpp
+++ Huff and Puff/Huff and Puff/src/huff.cpp
@@ -14,6 +14,20 @@
 
 using namespace std;
 
+const int MAX_GLYPHS = 256;
+const int EOF_GLYPH = -1;
+
+const int EOF_GLYPH_COUNT = 1;
+const int DEFAULT_NODE_POINTER = -1;
+
+struct HuffmanNode {
+
+	int glyph;
+	int frequency;
+	int left;
+	int right;
+};
+
 /******************************************************************************
 	Name: readFile
 	Des:
@@ -44,6 +58,64 @@ char* readFile(string fileName, int &oFileLength) {
 	return result;
 }
 
+/******************************************************************************
+	Name: generateInitialHuffmanTable
+	Des:
+		Builds the huffman table
+	Params:
+		data - type char *, the data for the huffman table
+		data - type int, the length of the data
+	Returns:
+		type vector<HuffmanNode>, the huffman table
+******************************************************************************/
+vector<HuffmanNode> generateInitialHuffmanTable(char* data, int dataLength) {
+
+	vector<int> frequencyTable(MAX_GLYPHS);
+
+	for (int i = 0; i < dataLength; i++) {
+
+		int glyph = data[i];
+
+		frequencyTable[glyph]++;
+	}
+
+	vector<HuffmanNode> result;
+
+	for (int i = 0; i < frequencyTable.size(); i++) {
+
+		// If the glyph has a frequency greater than zero
+		if (frequencyTable[i] > 0) {
+
+			HuffmanNode node;
+
+			node.glyph = i;
+			node.frequency = frequencyTable[i];
+			node.left = DEFAULT_NODE_POINTER;
+			node.right = DEFAULT_NODE_POINTER;
+
+			result.push_back(node);
+		}
+	}
+
+	// Add the EOF glyph to the table
+	HuffmanNode node;
+
+	node.glyph = EOF_GLYPH;
+	node.frequency = EOF_GLYPH_COUNT;
+	node.left = DEFAULT_NODE_POINTER;
+	node.right = DEFAULT_NODE_POINTER;
+
+	result.push_back(node);
+
+	// Sort by frequency
+	sort(result.begin(), result.end(), [](HuffmanNode a, HuffmanNode b) {
+
+		return a.frequency < b.frequency;
+		});
+
+	return result;
+}
+
 int main() {
 
 	string fileName;
@@ -57,6 +129,8 @@ int main() {
 	char* data = readFile(fileName, fileLength);
 
 	if (data != nullptr) {
+
+		vector<HuffmanNode> huffmanTable = generateInitialHuffmanTable(data, fileLength);
 
 		delete[fileLength] data;
 	}
